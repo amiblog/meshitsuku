8f10a1ea3ddec71d4213cff381731ba08f31f3c2
amiblog <reza.am.0425@gmail.com>
2022-03-05 09:27:17 +0000

    devise機能　ルート記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get 'homes/top' => 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update, :destroy]
+  end
+
+
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  scope module: :public do
+    get 'homes/top'
+    get 'homes/about'
+    resources :customers, only: [:index, :show, :edit, :update, :destroy]
+    resources :recipes, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
