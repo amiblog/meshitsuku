c9e29672de8df155e299fb23ee77025198e6629b
amiblog <reza.am.0425@gmail.com>
2022-03-06 12:54:22 +0000

    トップ、サインイン、ログイン画面編集

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  has_one_attached :recipe_image
+  
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  belongs_to :customer
+
 end
