e709c175b5235282d81042d9bacb4cad68696bff
amiblog <reza.am.0425@gmail.com>
2022-03-07 09:16:52 +0000

    [add]新規投稿機能

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -1,13 +1,41 @@
 class Public::RecipesController < ApplicationController
   def new
+    @recipe=Recipe.new
+  end
+  
+  def create
+    @recipe=Recipe.new(recipe_params)
+    @recipe.customer_id=current_customer.id
+    @recipe.save
+    redirect_to recipe_path(@recipe.id)
   end
 
   def index
+    @recipes=Recipe.all
   end
 
   def show
+    @recipe=Recipe.find(params[:id])
   end
 
   def edit
+    @recipe=Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe=Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe.id)
+  end
+  
+  def destroy
+    @recipe=Recipe.find(params[:id])
+    @recipe.destroy
+  end
+  
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:title, :body)
   end
 end
