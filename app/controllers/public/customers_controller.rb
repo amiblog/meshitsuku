c9da6ff06dfe7c07f0febada633e386efda9f1f8
amiblog <reza.am.0425@gmail.com>
2022-03-08 04:27:49 +0000

    [fix]deviseエラー

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,4 +10,16 @@ class Public::CustomersController < ApplicationController
   def edit
     @customer=Customer.find(params[:id])
   end
+
+  def update
+    @customer=Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customer_path(@customer.id)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :nickname, :introduction, :profile_image)
+  end
 end
